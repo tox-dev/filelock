57a541fb0bcedb9250877da8a5003a695f7f8aa9
Benedikt Schmitt <benedikt@benediktschmitt.de>
2016-11-05 18:44:06 +0100

    *fixed* issue #14 (moved license and readme file to MANIFEST)

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
     url = "https://github.com/benediktschmitt/py-filelock",
     download_url = "https://github.com/benediktschmitt/py-filelock/archive/master.zip",
     py_modules = ["filelock"],
-    data_files = [("", ["LICENSE.rst", "README.rst"])],
     license = license_,
     classifiers = [
         "License :: Public Domain",
