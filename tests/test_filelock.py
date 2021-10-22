64774ed61c1b802a6c8641f833a786a4d18e2e50
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-10-22 13:41:46 +0200

    Use pytest.parametrize to test across path types.

--- tests/test_filelock.py
+++ tests/test_filelock.py
@@ -13,25 +13,21 @@ from _pytest.logging import LogCaptureFixture
 from filelock import BaseFileLock, FileLock, SoftFileLock, Timeout
 
 
-@pytest.mark.parametrize("lock_type", [FileLock, SoftFileLock])
-def test_simple(lock_type: Type[BaseFileLock], tmp_path: Path, caplog: LogCaptureFixture) -> None:
+@pytest.mark.parametrize(
+    ("lock_type", "path_type"), [(FileLock, str), (FileLock, Path), (SoftFileLock, str), (SoftFileLock, Path)]
+)
+def test_simple(
+    lock_type: Type[BaseFileLock], path_type: Union[Type[str], Type[Path]], tmp_path: Path, caplog: LogCaptureFixture
+) -> None:
     caplog.set_level(logging.DEBUG)
 
     # test lock creation by passing a `str`
     lock_path = tmp_path / "a"
-    lock = lock_type(str(lock_path))
+    lock = lock_type(path_type(lock_path))
     with lock as locked:
         assert lock.is_locked
         assert lock is locked
     assert not lock.is_locked
-
-    # test creation by passing a `pathlib.Path`
-    lock_path_2 = tmp_path / "b"
-    lock_2 = lock_type(lock_path_2)
-    with lock_2 as locked:
-        assert lock_2.is_locked
-        assert lock_2 is locked
-    assert not lock_2.is_locked
 
     assert caplog.messages == [
         f"Attempting to acquire lock {id(lock)} on {lock_path}",
