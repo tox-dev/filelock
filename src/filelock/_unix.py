e63ada1e83040bf3d17ed441034ddaef9e21907c
Dirk Groeneveld <dirkg@allenai.org>
2022-03-23 09:06:49 -0700

    Fix race condition

--- src/filelock/_unix.py
+++ src/filelock/_unix.py
@@ -32,16 +32,27 @@ else:  # pragma: win32 no cover
 
         def _acquire(self) -> None:
             open_mode = os.O_RDWR | os.O_CREAT
-            fd = os.open(self._lock_file, open_mode)
-            try:
-                if os.fstat(fd).st_size == 0:
-                    os.write(fd, b"Lock files must not be empty, or the Google Drive app will replace them.")
-                    os.fsync(fd)
-                fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-            except OSError:
-                os.close(fd)
-            else:
-                self._lock_file_fd = fd
+            while True:
+                # Make sure we have a non-empty file.
+                fd = os.open(self._lock_file, open_mode)
+                try:
+                    if os.fstat(fd).st_size == 0:
+                        os.write(fd, b"Lock files must not be empty, or the Google Drive app will replace them.")
+                finally:
+                    os.close(fd)
+
+                fd = os.open(self._lock_file, open_mode)
+                try:
+                    if os.fstat(fd).st_size == 0:
+                        # Looks like Google Drive already replaced the file. Try again.
+                        os.close(fd)
+                        continue
+                    fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                except OSError:
+                    os.close(fd)
+                else:
+                    self._lock_file_fd = fd
+                break
 
         def _release(self) -> None:
             # Do not remove the lockfile:
