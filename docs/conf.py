1cf8fcae4e688c60a078b1ec9286637141fb35bf
Bernát Gábor <gaborjbernat@gmail.com>
2022-12-28 16:26:38 +0000

    Fix the CI, bump tools (#185)

--- docs/conf.py
+++ docs/conf.py
@@ -4,50 +4,22 @@ from datetime import date, datetime
 
 from filelock import __version__
 
-company = "tox-dev"
-name = "filelock"
-version = ".".join(__version__.split(".")[:2])
-release = __version__
+name, company = "filelock", "tox-dev"
+version, release = ".".join(__version__.split(".")[:2]), __version__
 copyright = f"2014-{date.today().year}, {company}"
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.viewcode",
     "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
     "sphinx_autodoc_typehints",
 ]
-
-templates_path = []
-unused_docs = []
-source_suffix = ".rst"
-exclude_patterns = ["_build"]
-
-master_doc = "index"
-pygments_style = "default"
-
-project = name
-today_fmt = "%B %d, %Y"
-
 html_theme = "furo"
-html_favicon = "logo.svg"
-html_logo = "logo.svg"
-html_theme_options = {
-    "navigation_with_keys": True,
-}
-html_title = name
-html_last_updated_fmt = datetime.now().isoformat()
-
-autoclass_content = "class"
-autodoc_member_order = "bysource"
-autodoc_default_options = {
-    "member-order": "bysource",
-    "undoc-members": True,
-    "show-inheritance": True,
-}
-autodoc_typehints = "none"
-always_document_param_types = False
-typehints_fully_qualified = True
+html_title, html_last_updated_fmt = name, datetime.now().isoformat()
+pygments_style, pygments_dark_style = "sphinx", "monokai"
+autoclass_content, autodoc_member_order, autodoc_typehints = "class", "bysource", "none"
+autodoc_default_options = {"member-order": "bysource", "undoc-members": True, "show-inheritance": True}
 autosectionlabel_prefix_document = True
 
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
