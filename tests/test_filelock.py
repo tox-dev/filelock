9bc2e910db1d465743524b4f4973fe3b0ed1e215
Mark Harfouche <mark.harfouche@gmail.com>
2023-10-30 13:20:01 -0400

    Lint

--- tests/test_filelock.py
+++ tests/test_filelock.py
@@ -12,7 +12,7 @@ from inspect import getframeinfo, stack
 from pathlib import Path, PurePath
 from stat import S_IWGRP, S_IWOTH, S_IWUSR, filemode
 from types import TracebackType
-from typing import TYPE_CHECKING, Callable, Iterator, Tuple, Type, Union
+from typing import TYPE_CHECKING, Any, Callable, Iterator, Tuple, Type, Union
 from uuid import uuid4
 
 import pytest
@@ -613,19 +613,35 @@ def test_lock_can_be_non_thread_local(
     lock.release(force=True)
 
 
-@pytest.mark.parametrize("lock_type", [FileLock, SoftFileLock])
-def test_subclass_compatibility(lock_type: type[BaseFileLock], tmp_path: Path) -> None:
-    class MyFileLock(lock_type):
-        def __init__(
+def test_subclass_compatibility(tmp_path: Path) -> None:
+    class MyFileLock(FileLock):
+        def __init__(  # noqa: PLR0913 Too many arguments to function call (6 > 5)
             self,
-            *args,  # noqa: ANN002
+            lock_file: str | os.PathLike[str],
+            timeout: float = -1,
+            mode: int = 0o644,
+            thread_local: bool = True,
             my_param: int = 0,
-            **kwargs,  # noqa: ANN003
+            **kwargs: dict[str, Any],
         ) -> None:
             pass
 
     lock_path = tmp_path / "a"
     MyFileLock(str(lock_path), my_param=1)
+
+    class MySoftFileLock(SoftFileLock):
+        def __init__(  # noqa: PLR0913 Too many arguments to function call (6 > 5)
+            self,
+            lock_file: str | os.PathLike[str],
+            timeout: float = -1,
+            mode: int = 0o644,
+            thread_local: bool = True,
+            my_param: int = 0,
+            **kwargs: dict[str, Any],
+        ) -> None:
+            pass
+
+    MySoftFileLock(str(lock_path), my_param=1)
 
 
 @pytest.mark.parametrize("lock_type", [FileLock, SoftFileLock])
