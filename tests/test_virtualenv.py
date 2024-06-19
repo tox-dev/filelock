81d4cf9e9ea98cefdf9f380b024d627dffb28efb
Bernát Gábor <gaborjbernat@gmail.com>
2024-06-19 07:38:03 -0700

    Add test for virtualenv stability (#344)

---
+++ tests/test_virtualenv.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from virtualenv import cli_run
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def test_virtualenv(tmp_path: Path) -> None:
+    cli_run([str(tmp_path), "--no-pip", "--no-setuptools", "--no-periodic-update"])
